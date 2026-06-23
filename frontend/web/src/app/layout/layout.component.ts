2b3c0adf620a6f208a8976949ece7cb7a9426bf6
ronalete <ronald.medina.corp@gmail.com>
2026-06-23 16:34:23 -0500

    fix: add @angular/animations dep, remove unused NgIf/AsyncPipe in layout

--- frontend/web/src/app/layout/layout.component.ts
+++ frontend/web/src/app/layout/layout.component.ts
@@ -1,12 +1,11 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { NgIf, AsyncPipe } from '@angular/common';
 import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIf, AsyncPipe],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
